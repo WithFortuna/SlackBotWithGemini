dc8cf4a41556cc198d13c045f2d55f48bd13916e
KeunHo Choi <rmsghchl0@gmail.com>
2025-10-02 10:50:26 +0900

    chore: mysql 연결

--- src/main/java/com/zuzihe/slackbot/SlackbotApplication.java
+++ src/main/java/com/zuzihe/slackbot/SlackbotApplication.java
@@ -2,10 +2,14 @@ package com.zuzihe.slackbot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+@EnableJpaAuditing
+@EnableJpaRepositories(basePackages = "com.zuzihe.slackbot.slack.workspace.repository")
 public class SlackbotApplication {
 
     public static void main(String[] args) {
